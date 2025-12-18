aecde59927b9705550c718f0214823c2631ccc05
haha27lim <eileen2708@hotmail.com>
2025-12-18 14:21:14 +0800

    must add dependency Jackson JSON to pom.xml and amend AppConstants

--- src/main/java/com/example/KafkaJsonSerializerAndJsonDeserializer/utils/AppConstants.java
+++ src/main/java/com/example/KafkaJsonSerializerAndJsonDeserializer/utils/AppConstants.java
@@ -3,5 +3,5 @@ package com.example.KafkaJsonSerializerAndJsonDeserializer.utils;
 public class AppConstants {
 
     public static final String TOPIC_NAME = "my-topic";
-    public static final String GROUP_ID = "group_id";
+    public static final String GROUP_ID = "group-id";
 }
